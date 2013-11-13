ee782759f3074a057dfede3a81596dd7bf4f50b6
Alexander Profeit <aprofeit@me.com>
2013-11-13 04:03:02 +0000

    ignore keys during precompilation

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,7 @@
-uri                     = URI.parse(Keys.redistogo_url)
-redis_params            = { host: uri.host, port: uri.port }
-redis_params[:password] = uri.password if Rails.env.production?
+if Keys.redistogo_url
+  uri                     = URI.parse(Keys.redistogo_url)
+  redis_params            = { host: uri.host, port: uri.port }
+  redis_params[:password] = uri.password if Rails.env.production?
 
-Redis.current = Redis.new(redis_params)
+  Redis.current = Redis.new(redis_params)
+end
