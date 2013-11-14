9b5daed90093f5f4d6a113bc02200cc0727efa50
Alexander Profeit <aprofeit@me.com>
2013-11-14 03:34:12 +0000

    update keys.sample, add presence validation

--- app/models/notification_recipient.rb
+++ app/models/notification_recipient.rb
@@ -1,5 +1,5 @@
 class NotificationRecipient < ActiveRecord::Base
   PHONE_REGEX = /\+1\d{10}/ # +18195551234
 
-  validates :phone, format: { with: PHONE_REGEX }
+  validates :phone, format: { with: PHONE_REGEX }, presence: true
 end
